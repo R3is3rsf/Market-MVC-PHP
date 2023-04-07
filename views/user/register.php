6396c34069bfd25d979fb2674da24b93fda4a40e
r3is3rsf <robertoperez297@gmail.com>
2023-04-07 19:18:04 -0400

    Fixed validate form

--- views/user/register.php
+++ views/user/register.php
@@ -17,21 +17,24 @@
     <form action="<?=base_url?>user/save" method="POST">
         <?php if(isset($_SESSION['error']['name'])):  ?>
            <h4 class="text-danger"><?= $_SESSION['error']['name'];?></h4> 
-        <?php endif; ?>    
+           
+        <?php 
+           endif;
+         ?>    
         <label class="form-label" for="name">Name</label>
         <input class="form-control" type="text" name="name" id="name" placeholder="Write your"  >
-        <?php if(isset($_SESSION['error']['name'])):  ?>
+        <?php if(isset($_SESSION['error']['lastname'])):  ?>
            <h4 class="text-danger"><?= $_SESSION['error']['lastname'];?></h4> 
         <?php endif; ?>   
         <label class="form-label" for="lastname">Lastname</label>
         <input class="form-control" type="text" name="lastname" id="lastname" placeholder="Write your lastname" required>
-        <?php if(isset($_SESSION['error']['name'])):  ?>
-           <h4 class="text-danger"><?= $_SESSION['error']['name'];?></h4> 
+        <?php if(isset($_SESSION['error']['email'])):  ?>
+           <h4 class="text-danger"><?= $_SESSION['error']['email'];?></h4> 
         <?php endif; ?>       
         <label class="form-label" for="email">Email</label>
         <input class="form-control" type="email" name="email" id="email" placeholder="example@gmail.com" required>
-        <?php if(isset($_SESSION['error']['name'])):  ?>
-           <h4 class="text-danger"><?= $_SESSION['error']['name'];?></h4> 
+        <?php if(isset($_SESSION['error']['password'])):  ?>
+           <h4 class="text-danger"><?= $_SESSION['error']['password'];?></h4> 
         <?php endif; ?>       
         <label for="password">Password</label>
         <input class="form-control" type="password" name="password" id="password" placeholder="Your password here" required>
@@ -39,5 +42,6 @@
         <input class="form-control" type="password" name="repassword" id="repassword" placeholder="Repeat your password" required>
 
         <button class="btn btn-primary mt-2">Register</button>
+        <?php  Utils::deleteSesion('error');  ?>
        </form>
 </div>
